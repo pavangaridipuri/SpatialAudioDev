3f1c67bf8034ad9a4702f704b9eee00f79c0fbe8
Pavan <pgaridipuri@gmail.com>
2021-05-05 12:41:26 -0700

    emergency push

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var http = require('http');
 var fs = require('fs');
 var index = fs.readFileSync( 'index.html');
 var counter=0;
+var orientation;
 var SerialPort = require('serialport');
 const parsers = SerialPort.parsers;
 var sys = require('sys'),
@@ -10,7 +11,7 @@ const parser = new parsers.Readline({
     delimiter: '\r\n'
 });
 
-var port = new SerialPort('/dev/cu.usbmodem14401',{ 
+var port = new SerialPort('/dev/cu.usbmodem14101',{ 
     baudRate: 9600,
     dataBits: 8,
     parity: 'none',
@@ -31,6 +32,9 @@ io.on('connection', function(socket) {
     
     console.log('Node is listening to port');
     exec('osascript -e "set volume output volume 100"', {silent:true}).output;
+    // exec('osascript FixSoundBalance.applescript', {silent:true}).output;
+
+
     // exec('open https://www.youtube.com/watch?v=gQYsUjT-IBo', {silent:true}).output;
 
 });
@@ -38,8 +42,27 @@ io.on('connection', function(socket) {
 parser.on('data', function(data) {
     
     console.log('Received data from port: ' + data);
-    var myArray = data.split(" ");
-    exec('osascript -e "set volume output volume '+((myArray[0]-24.5)*200)+'\"', {silent:true}).output; 
+    var myArray = data.split("/");
+    // exec('osascript -e "set volume output volume '+((myArray[0]-24.5)*200)+'\"', {silent:true}).output; 
+    if(orientation!=myArray[4])
+    {
+        let cmd;
+        orientation=myArray[4];
+        if(orientation="Left")
+        {
+            cmd = `osascript FixSoundBalanceLeft.applescript`
+        }
+        else if(orientation="Right")
+        {
+            cmd = `osascript FixSoundBalanceRight.applescript`
+        }        
+        else if(orientation="Center"){
+            cmd = `osascript FixSoundBalanceCenter.applescript`
+        }        
+        require('child_process').exec(cmd, function (error, stdout, stderr) { console.log(error) })
+
+    }
+
     setTimeout(function(){ console.log("waiting"); }, 6000);
     io.emit('data', data);
     
